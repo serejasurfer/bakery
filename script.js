7b40935c065516d24106c9d2441bddc494bd0db1
Danil-Zz <klishta.danil@yandex.ru>
2023-11-04 13:50:33 +0500

    Improve view

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const navBar = document.querySelector("nav"),
-              menuBtns = document.querySelectorAll(".menu-icon"),
-              overlay = document.querySelector(".overlay");
+      menuBtns = document.querySelectorAll(".menu-icon"),
+      overlay = document.querySelector(".overlay");
         
-              menuBtns.forEach(menuBtn => {
-                menuBtn.addEventListener("click", () => {
-                    navBar.classList.toggle("open")
-                })
-              })
+menuBtns.forEach(menuBtn => {
+  menuBtn.addEventListener("click", () => {
+      navBar.classList.toggle("open")
+  })
+})
 
-              overlay.addEventListener("click", () => {
-                navBar.classList.remove("open")
-              })
+overlay.addEventListener("click", () => {
+  navBar.classList.remove("open")
+})
