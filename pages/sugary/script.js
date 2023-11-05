c1411c5c2ccebda470e79acac61cbc487a0f6e8c
serejasurfer <serejasurfer@gmail.com>
2023-11-06 02:05:26 +0500

    Изменил относительные пути в htmlах

--- pages/sugary/script.js
+++ pages/sugary/script.js
@@ -11,3 +11,25 @@ menuBtns.forEach(menuBtn => {
 overlay.addEventListener("click", () => {
   navBar.classList.remove("open")
 })
+// POPUP
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.card');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupBg.classList.add('active');
+  })
+})
+
+closePopupButton.addEventListener('click',() => {
+  popupBg.classList.remove('active');
+});
+
+document.addEventListener('click',(e) => {
+  if(e.target === popupBg) {
+    popupBg.classList.remove('active');
+  }
+});
